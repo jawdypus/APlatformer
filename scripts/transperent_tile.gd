1262c38e16269d82afcf3c9131644bce72c2586d
jawdypus <auditoreven@gmail.com>
2024-08-08 09:19:35 +0300

    added 3rd level

--- scripts/transperent_tile.gd
+++ scripts/transperent_tile.gd
@@ -1,10 +1,8 @@
 extends TileMap
 
-func _on_area_2d_body_entered(body):
-	if body.get_name() == "Player":
-		modulate.a = 0.5
+func _on_area_2d_body_entered(body: CharacterBody2D):
+	modulate.a = 0.5
 
 
-func _on_area_2d_body_exited(body):
-	if body.get_name() == "Player":
-		modulate.a = 1
+func _on_area_2d_body_exited(body: CharacterBody2D):
+	modulate.a = 1
